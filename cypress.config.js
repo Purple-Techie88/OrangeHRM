a0e26ab834d1902e647ad7743d48524b0d34fb17
LaMorica.Swain <lamorica.swain@calendly.com>
2024-01-09 19:54:55 -0500

    add cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  env: {
+    userName: "Admin",
+    password: "admin123",
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "https://opensource-demo.orangehrmlive.com",
+    specPattern: "cypress/e2e/**/*_spec.js",
+    scrollBehavior: "center",
   },
 });
